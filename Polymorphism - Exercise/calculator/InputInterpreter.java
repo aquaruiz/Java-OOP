5f96dc3fbba65c96284e4556a23947575b80d77a
aquariuz <gpetiaa@gmail.com>
2019-03-25 22:15:00 +0200

    solved last part of Polymorphism exercise

---
+++ Polymorphism - Exercise/calculator/InputInterpreter.java
@@ -0,0 +1,47 @@
+//package calculator;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class InputInterpreter {
+    private CalculationEngine engine;
+    private Deque<Integer> memoryStack;
+    
+    public InputInterpreter(CalculationEngine engine){
+        this.engine = engine;
+        this.memoryStack = new ArrayDeque<>();
+    }
+
+    public boolean interpret(String input) {
+    	// memory save
+    	if (input.equals("ms")) {
+			this.memoryStack.push(engine.getCurrentResult());
+			return true;
+		}
+    	
+    	if (input.equals("mr")) {
+			engine.pushNumber(this.memoryStack.pop());
+			return true;
+		}
+    	
+        try {
+            engine.pushNumber(Integer.parseInt(input));
+        }catch (Exception ex){
+            engine.pushOperation(this.getOperation(input));
+        }
+        
+        return true;
+    }
+    
+    public Operation getOperation(String operation) {
+        if (operation.equals("*")) {
+            return new MultiplicationOperation();
+        } 
+        
+        if (operation.equals("/")) {
+        	return new DevisionOperation();
+        }
+        
+        return null;
+    }
+}
---
+++ Polymorphism - Exercise/calculator/Main.java
@@ -0,0 +1,25 @@
+//package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CalculationEngine engine = new CalculationEngine();
+        InputInterpreter interpreter = Extensions.buildInterpreter(engine);
+        String[] tokens = scanner.nextLine().split("\\s+");
+        
+        scanner.close();
+        
+        for (String token : tokens) {
+            if(token.equals("end")){
+                break;
+            }
+            
+            interpreter.interpret(token);
+        }
+
+        System.out.println(engine.getCurrentResult());
+    }
+}
